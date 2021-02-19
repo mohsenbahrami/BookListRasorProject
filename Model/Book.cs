b9bd2904c18d81e6eecebd732266620972e2078e
mohsenbahrami <mohsenbahrami_ah@yahoo.com>
2021-02-18 21:59:57 -0500

    initail code for the project

---
+++ Model/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRasor.Model
+{
+    public class Book
+    {
+        [Key]
+        public int MyProperty { get; set; }
+
+        [Required]
+        public String Name { get; set; }
+        public String  Author { get; set; }
+    }
+}
